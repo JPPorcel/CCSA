43b4116cec6cdf012f48ad6b6776273898d641a2
JPPorcel <juanpiporcel@gmail.com>
2017-03-21 16:50:41 +0100

    templates with pug

--- server.js
+++ server.js
@@ -2,65 +2,23 @@ var express = require("express"),
     app = express(),
     bodyParser  = require("body-parser"),
     methodOverride = require("method-override");
-    mysql = require('mysql');
+	
+	
+app.set('view engine', 'pug')
 
 app.use(bodyParser.urlencoded({ extended: false }));  
 app.use(bodyParser.json());  
 app.use(methodOverride());
 
-var connection = mysql.createConnection({
-  host     : '192.168.10.92',
-  user     : 'music',
-  password : 'music',
-  database : 'Music'
-});
-
 var router = express.Router();
 
 router.get('/', function(req, res) 
 {  
-	res.send("Server Music");
+	res.render(
+        'index',
+        { title: 'Hey Hey Hey!', message: 'Yo Yo'})
 });
 
-// devolverá lista de marchas
-router.get('/marchas', function(req, res) 
-{  
-	connection.query("select * from Marchas",function(err,rows){
-		if(err) 
-			throw err;
-		
-		res.set({ 'content-type': 'application/json; charset=utf-8' });
-		res.send(rows);
-	});
-});
-
-// devuelve la información de una marcha, si existe
-router.get('/marcha/info/:id', function(req, res) 
-{  
-	connection.query("select * from Marchas where id='" + req.params.id + "'",function(err,rows)
-	{
-		if(err) 
-			throw err;
-		
-		res.set({ 'content-type': 'application/json; charset=utf-8' });
-		res.send(rows[0]);
-	});
-});
-
-
-router.get('/filtro/:filtro', function(req, res)
-{
-	connection.query("select * from Marchas where titulo like '%" + req.params.filtro + "%' or autor like '%" + req.params.filtro + "%'", function (err, rows)
-	{
-		if(err)
-			throw err;
-		
-		console.log(req.params.filtro);
-		
-		res.set({ 'content-type': 'application/json; charset=utf-8' });
-		res.send(rows);
-	});
-});
 
 app.use(router);
 
